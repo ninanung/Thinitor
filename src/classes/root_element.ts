4ac03b088963913dbbca51d84232fa880c80d79e
Jun Seungjae <ninanung0503@gmail.com>
2020-11-15 21:28:13 +0900

    fixed error, need to control enter event int the root element

--- src/classes/root_element.ts
+++ src/classes/root_element.ts
@@ -6,26 +6,31 @@ import { logError } from '../utils/errorLogs';
 import TextElement from './text_element';
 
 class RootElement {
+    private rootElement: Element;
     private element: Element;
-    private children: Array<Element>;
-    private classes: Array<TextElement>
+    private children: Array<Element> = [];
+    private classes: Array<TextElement> = [];
 
     constructor(id: string) {
         const element = document.getElementById(id);
         if (!element) logError(`Can't find the element has "${id}" id.`);
         else {
-            listenEnterKeyInTheRoot(element);
+            this.rootElement = element;
             this.element = element;
             const newElement = new TextElement(generateRandomString());
             this.addNewChild(newElement);
+            listenEnterKeyInTheRoot(this);
         }
     }
 
     private addNewChild(child: TextElement) {
-        console.log(this.getElement());
         this.element.appendChild(child.getElement());
         this.children = [...this.children, child.getElement()];
         this.classes = [...this.classes, child];
+    }
+
+    public getRootElement(): Element {
+        return this.rootElement;
     }
 
     public getHtml(): string {
--- src/modules/event_listener/root_element_key_listener.ts
+++ src/modules/event_listener/root_element_key_listener.ts
@@ -2,14 +2,17 @@ import { createElement } from '../element_creator/create_element';
 
 import { generateRandomString } from '../../utils/common';
 
+import RootElement from '../../classes/root_element';
+
 // this function has no return
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const listenEnterKeyInTheRoot = (element: Element) => {
-    element.addEventListener('keydown', (e: KeyboardEvent) => {
+export const listenEnterKeyInTheRoot = (root: RootElement) => {
+    const listenElement = root.getElement();
+    listenElement.addEventListener('keydown', (e: KeyboardEvent) => {
         if (e.key === 'Enter') {
             e.preventDefault;
             const newPElement = createElement(generateRandomString(), 'p');
-            element.appendChild(newPElement);
+            listenElement.appendChild(newPElement);
 
             // TODO 이 경우 단순히 element만 생성하는게 아니라 
             // element의 정보를 가지고 있는 class객체를 생성해서 컨트롤 하는편이 좋을 것 같음
