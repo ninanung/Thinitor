31ff72823f7f192b0354de40b8bb5967ce5f5fb7
Jun Seungjae <ninanung0503@gmail.com>
2020-11-24 22:47:11 +0900

    contentEditable caret control module

--- src/modules/event_listener/text_element_keyborad_event.ts
+++ src/modules/event_listener/text_element_keyborad_event.ts
@@ -1,15 +1,29 @@
 import TextElement from '../../classes/text_element';
+import { createElement } from '../element_creator/create_element';
 
 export const listenArrowKeyInTheText = (child: TextElement): void => {
     const listenElement = child.getElement();
     listenElement.addEventListener('keydown', (e: KeyboardEvent) => {
         if (e.key === 'ArrowUp') {
             const previousChildElement = document.getElementById(child.getElementId()).previousElementSibling
-            if (previousChildElement) document.getElementById(previousChildElement.id).focus();
+            if (previousChildElement) {
+                const tempElement = createElement('temp_focus_el', 'span');
+                previousChildElement.appendChild(tempElement)
+                const newRange = document.createRange();
+                newRange.setStart(previousChildElement.childNodes[previousChildElement.childNodes.length - 1], 0);
+                newRange.collapse(true);
+                const selection = document.getSelection();
+                selection.removeAllRanges();
+                selection.addRange(newRange);
+                // document.getElementById(previousChildElement.id).focus();
+                // TODO
+            }
         }
         if (e.key === 'ArrowDown') {
             const nextChildElement = document.getElementById(child.getElementId()).nextElementSibling;
-            if (nextChildElement) document.getElementById(nextChildElement.id).focus();
+            if (nextChildElement) {
+                document.getElementById(nextChildElement.id).focus();
+            }
         }
     });
 }
