023c464a9f800e78229f7fcee0b3bd1aeb7250e6
Jun Seungjae <ninanung0503@gmail.com>
2020-11-26 23:00:11 +0900

    still fixing caret control issue...

--- src/modules/event_listener/text_element_keyborad_event.ts
+++ src/modules/event_listener/text_element_keyborad_event.ts
@@ -1,5 +1,5 @@
 import TextElement from '../../classes/text_element';
-import { createElement } from '../element_creator/create_element';
+// import { createElement } from '../element_creator/create_element';
 
 export const listenArrowKeyInTheText = (child: TextElement): void => {
     const listenElement = child.getElement();
@@ -7,15 +7,14 @@ export const listenArrowKeyInTheText = (child: TextElement): void => {
         if (e.key === 'ArrowUp') {
             const previousChildElement = document.getElementById(child.getElementId()).previousElementSibling
             if (previousChildElement) {
-                const tempElement = createElement('temp_focus_el', 'span');
-                previousChildElement.appendChild(tempElement)
                 const newRange = document.createRange();
-                newRange.setStart(previousChildElement.childNodes[previousChildElement.childNodes.length - 1], 0);
+                newRange.setStart(previousChildElement.childNodes[previousChildElement.childNodes.length - 1], 2);
                 newRange.collapse(true);
                 const selection = document.getSelection();
                 selection.removeAllRanges();
                 selection.addRange(newRange);
-                // document.getElementById(previousChildElement.id).focus();
+                // 코드 확인결과 동작 자체에는 이상이 없지만 keydown이벤트 후 range의 변경이 이루어지지 않는 이슈가 있어서 확인이 필요
+                // 혹은 createElement로 텍스트 엘리먼트를 생성하고 append해서 해당 element의 nodeValue를 임시로 넣었다가 제거하는 방법도 있을듯
                 // TODO
             }
         }
